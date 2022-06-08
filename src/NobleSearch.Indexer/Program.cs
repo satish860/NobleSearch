e45c801f770cc9e4b434dafd48c658cf1b016fa2
satish <satish860@gmail.com>
2022-06-08 15:51:21 +0530

    Indexing with the data.json

--- src/NobleSearch.Indexer/Program.cs
+++ src/NobleSearch.Indexer/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NobleSearch.Model;
+using Redis.OM;
+using System.Text.Json;
+
+// Get the data from the File attached to the process
+string jsonString = File.ReadAllText("Noble.json");
+List<Person>? persons = JsonSerializer.Deserialize<List<Person>>(jsonString);
+
+// Connect to the Redis(docker) which is running locally
+var provider = new RedisConnectionProvider("redis://localhost:6379");
+var connection = provider.Connection;
+
+// Create a the index with the class(data) model created
+connection.CreateIndex(typeof(Person));
+var personCollection = provider.RedisCollection<Person>();
+
+// Insert the data and the data will automatically get inserted into the database
+List<Task> tasks = new List<Task>();
+foreach (var item in persons)
+{
+    tasks.Add(personCollection.InsertAsync(item));
+}
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine("Indexed");
+
+
+
---
+++ src/NobleSearch.Model/Person.cs
@@ -0,0 +1,37 @@
+﻿using Redis.OM.Modeling;
+using System.Text.Json.Serialization;
+
+namespace NobleSearch.Model
+{
+    [Document(IndexName = "Person-idx", StorageType = StorageType.Json)]
+    public class Person
+    {
+        [JsonPropertyName("id")]
+        [RedisIdField]
+        public string Id { get; set; }
+
+        [JsonPropertyName("year")]
+        [Indexed]
+        public string Year { get; set; }
+
+        [JsonPropertyName("category")]
+        [Searchable(Aggregatable = true)]
+        public string Category { get; set; }
+
+        [JsonPropertyName("firstname")]
+        [Searchable]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("surname")]
+        [Searchable]
+        public string Surname { get; set; }
+
+        [JsonPropertyName("motivation")]
+        [Searchable]
+        public string Motivation { get; set; }
+
+        [JsonPropertyName("share")]
+        [Indexed]
+        public string Share { get; set; }
+    }
+}

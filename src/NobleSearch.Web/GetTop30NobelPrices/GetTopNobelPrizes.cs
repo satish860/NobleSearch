8708eb0f89da34df339d2bfc0e9c543665564873
satish <satish860@gmail.com>
2022-06-09 13:19:42 +0530

    Added the Redis Connection Provider and initial pages

---
+++ src/NobleSearch.Web/GetTop30NobelPrices/GetTopNobelPrizes.cs
@@ -0,0 +1,31 @@
+﻿using FastEndpoints;
+using NobleSearch.Model;
+using Redis.OM;
+using Redis.OM.Contracts;
+
+namespace NobleSearch.Web.GetTop30NobelPrices
+{
+    public class GetTopNobelPrizes : Endpoint<EmptyRequest, List<Person>>
+    {
+        private readonly RedisConnectionProvider redisConnectionProvider;
+
+        public GetTopNobelPrizes(RedisConnectionProvider redisConnectionProvider)
+        {
+            this.redisConnectionProvider = redisConnectionProvider;
+        }
+
+        public override void Configure()
+        {
+            Verbs(Http.GET);
+            Routes("/api/noblewinners");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(EmptyRequest req, CancellationToken ct)
+        {
+            var provider = this.redisConnectionProvider.RedisCollection<Person>();
+            var collection = provider.Skip(1).Take(30).ToList();
+            await SendAsync(collection);
+        }
+    }
+}
--- src/NobleSearch.Web/Program.cs
+++ src/NobleSearch.Web/Program.cs
@@ -1,11 +1,11 @@
+using FastEndpoints;
+using Redis.OM;
+using Redis.OM.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFastEndpoints();
+builder.Services.AddSingleton(new RedisConnectionProvider("redis://localhost:6379"));
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
-
+app.UseFastEndpoints();
 app.Run();
